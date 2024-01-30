999d95bfb97e7afb3507dc7931720536e9cec16c
saaid323 <saaid2538@gmail.com>
2024-01-30 16:53:59 +0000

    added 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''implements LRU Caching'''
+from baseCache import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''class that inherits from BaseCaching and is a caching system'''
+
+    def __init__(self):
+        super().__init__()
+
+    def first_key(self, data):
+        '''return the first key'''
+        myList = [i for i in self.cache_data.keys()]
+        return myList[0]
+
+    def put(self, key, item):
+        '''puts items in a dictionary'''
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if self.cache_data.get(key) is not None:
+                del self.cache_data[key]
+                self.cache_data[key] = item
+            else:
+                first = self.first_key(self.cache_data)
+                discard = self.cache_data.pop(self.first_key(self.cache_data))
+                print(f'DISCARD: {first}')
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''gets item from dictionary'''
+        if self.cache_data.get(key) is None:
+            return None
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
+        return self.cache_data[key]
