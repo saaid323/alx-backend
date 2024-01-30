c378b4cbbcb14be5a56387420c22d918f946a589
saaid323 <saaid2538@gmail.com>
2024-01-30 13:12:48 +0000

    added 2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''implements Lifo cache system'''
+from baseCache import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''inherits from BaseCaching and is a caching system'''
+    lis = []
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        '''adds item to a dictionary'''
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if self.cache_data.get(key) is not None:
+                self.cache_data.pop(key)
+                self.cache_data[key] = item
+            else:
+                discard = self.cache_data.popitem()
+                print(f'DISCARD: {discard[0]}')
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+        return self.cache_data
+
+    def get(self, key):
+        '''gets item from dictionary'''
+        if self.cache_data[key] is None:
+            return None
+        return self.cache_data[key]
