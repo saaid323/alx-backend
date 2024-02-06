0ea0144def02f69333ac04d2a288ae59ab67e209
saaid323 <saaid2538@gmail.com>
2024-02-06 13:57:27 +0000

    added 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+'''flask 4-app.py'''
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -6,6 +8,7 @@ app = Flask(__name__)
 
 
 class Config:
+    '''config class'''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -17,6 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    '''get_local method'''
     lang = request.args.get('locale')
     supported = app.config['LANGUAGES']
     if lang in supported:
@@ -26,6 +30,7 @@ def get_locale():
 
 @app.route('/')
 def index():
+    '''index page of the app'''
     return render_template('3-index.html')
 
 
